aa72936f665ce25735318458384a9e1db73ca42e
Victor Skriplyuk <70595707+VitekSkrip@users.noreply.github.com>
2020-11-03 17:41:23 +0300

    Now_start_working

--- Lab1_VictorSkriplyuk/Lab1_VictorSkriplyuk/Lab1_VictorSkriplyuk.cpp
+++ Lab1_VictorSkriplyuk/Lab1_VictorSkriplyuk/Lab1_VictorSkriplyuk.cpp
@@ -9,7 +9,6 @@ struct Pipe
 	float length;
 	float diam;
 	bool repair;
-	bool exist = false;
 };
 
 struct CS
@@ -19,260 +18,195 @@ struct CS
 	int amount;
 	int amount_work;
 	float perfomance;
-	bool exist = false;
 };
+
+
+template <typename T>
+bool IsCorrect(T& var, int left, int right)
+{
+	return var >= left && var <= right;
+}
 
 template <typename T>       // шаблон "template" - для работы с обобщенным типом данных
 							//нашел по ссылке - http://cppstudio.com/post/5188/ 
-void checking(T& var, string com)
+void checking(T& var, string com, int left,int right)
 {
 	do
 	{
 		cin.clear();
-		cin.ignore(1000, '\n');
+		cin.ignore(10000, '\n');
 		cout << com;
 		cin >> var;
-	} while (var < 0 || cin.fail());
+	} while (cin.fail() || !IsCorrect(var,left,right));
 }
 
-void addpipe(Pipe& pipe1)
+int GetCorrectNumber(int left, int right)
 {
-	string x;
-
-	x = "Введите идентификатор: ";
-	checking(pipe1.id,x);
-
-	x = "Введите длину: ";
-	checking(pipe1.length,x);
-
-	x = "Введите диаметр: ";
-	checking(pipe1.diam,x);
-
-	x = "Ремонт: ";
-	checking(pipe1.repair, x);
-
-	pipe1.exist = true;
-
+	int x;
+	while (((cin >> x)).fail() || !IsCorrect(x, left, right))
+	{
+		cin.clear();
+		cin.ignore(10000, '\n');
+		cout << "Ошибка при вводе!" << endl << "Введите команду: ";
+	}
+	return x;
 }
 
-void addcs(CS& cs1)
-{
-	string x;
-	string str;
-	x = "Введите идентификатор: ";
-	checking(cs1.id,x);
 
+void addpipe(Pipe& pipe)
+{
+	checking(pipe.id,"Введите идентификатор: ",0,100000);
+	checking(pipe.length, "Введите длину: ",0,200);
+	checking(pipe.diam, "Введите диаметр: ",0,200);
+	checking(pipe.repair, "Ремонт: ",0,1);
+}
+
+void addcs(CS& cs)
+{
+	checking(cs.id, "Введите идентификатор: ",0, 10000);
 	do
 	{ 
 		cin.clear();
 		cin.ignore(1000, '\n');
 		cout << "Введите название: ";
-		getline(cin, cs1.name);  //нашел по ссылке: http://espressocode.top/getline-string-c/
+		getline(cin, cs.name);  //нашел по ссылке: http://espressocode.top/getline-string-c/
 								// нужно для того, чтобы считывало не одно слово - но не знаю, почему пропускает одну строчку в консоли
 	} while (cin.fail());
 
-	x = "Введите количество цехов: ";
-	checking(cs1.amount,x);
-
-	do {
-		cin.clear();
-		cin.ignore(1000, '\n');
-		cout << "Введите количество цехов в работе: ";
-		cin >> cs1.amount_work;
-	} while (cs1.amount_work>cs1.amount || cs1.amount_work< 0 || cin.fail());
-
-	do {
-		cin.clear();
-		cin.ignore(1000, '\n');
-		cout << "Введите эффективность: ";
-		cin >> cs1.perfomance;
-	} while (cs1.perfomance<0 || cs1.perfomance>1 || cin.fail());
-	
-	cs1.exist = true;
-
-
+	checking(cs.amount, "Введите количество цехов: ",0,1000);
+	checking(cs.amount_work, "Введите количество цехов в работе: ", 0, cs.amount);
+	checking(cs.perfomance, "Введите эффективность: ", 0, 1);
 }
 
-void view(Pipe pipe1, CS cs1)
+void view_pipe(Pipe pipe)
 {
-	if (pipe1.exist == true)
-	{
 		cout << endl;
 		cout << "Труба" << endl;
-		cout << "Идентификатор: " << pipe1.id << endl;
-		cout << "Длина: " << pipe1.length << endl;
-		cout << "Диаметр: " << pipe1.diam << endl;
-		cout << "Ремонт: " << pipe1.repair << endl;
-	}
-	else
-	{
-		cout << "Вы забыли ввести данные для трубы!" << endl;
-	}
-	if (cs1.exist == true)
+		cout << "Идентификатор: " << pipe.id << endl;
+		cout << "Длина: " << pipe.length << endl;
+		cout << "Диаметр: " << pipe.diam << endl;
+		cout << "Ремонт: " << pipe.repair << endl;
+}
+
+void view_cs(CS cs)
 	{
 		cout << endl;
 		cout << "Компрессорная станция" << endl;
-		cout << "Идентификатор: " << cs1.id << endl;
-		cout << "Название: " << cs1.name << endl;
-		cout << "Количество цехов: " << cs1.amount << endl;
-		cout << "Количество рабочих цехов: " << cs1.amount_work << endl;
-		cout << "Эффективность: " << cs1.perfomance << endl;
+		cout << "Идентификатор: " << cs.id << endl;
+		cout << "Название: " << cs.name << endl;
+		cout << "Количество цехов: " << cs.amount << endl;
+		cout << "Количество рабочих цехов: " << cs.amount_work << endl;
+		cout << "Эффективность: " << cs.perfomance << endl;
 	}
-	else
-	{
-		cout << "Вы забыли ввести данные для КС!" << endl;
-	}
-}
 
-void editing(Pipe& pipe1)
-{
-	if (pipe1.exist == true)
+void editing_pipe(Pipe& pipe)
 	{
-		string x;
-		cout << endl;
-
-		x = "Ремонт: ";
-		checking(pipe1.repair, x);
+		checking(pipe.repair, "Ремонт: ",0,1);
 	}
-	else
-	{
-		cout << "Вы забыли ввести данные для трубы" << endl;
-	}
-}
 
-void ceh(CS& cs1)
-{
-	if (cs1.exist == true)
+void editing_cs(CS& cs)
 	{
-		cout << "Запуск цеха - 1; Остановка цеха - 0" << endl;
 		int x;
-		cin >> x;
+		checking(x, "Запуск цеха - 1; Остановка цеха - 0\n", 0, 1);
 		if (x == 1)
 		{
-			if (cs1.amount <= cs1.amount_work)
+			if (cs.amount_work + 1 <= cs.amount)
 			{
-				cout << "Ошибка" << endl;
+				cs.amount_work++;
+				cout << "Новое количество рабочих цехов: " << cs.amount_work << endl;
 			}
 			else
 			{
-				cs1.amount_work++;
-				cout << "Новое количество рабочих цехов: " << cs1.amount_work << endl;
+				cout << "Нельзя запустить цех!" << endl;
 			}
 		}
 		else
 		{
-			if (cs1.amount < cs1.amount_work)
+			if (cs.amount_work-1 >= 0)
 			{
-				cout << "Ошибка" << endl;
+				cs.amount_work--;
+				cout << "Новое количество рабочих цехов: " << cs.amount_work << endl;
 			}
 			else
 			{
-				if (cs1.amount_work > 0)
-				{
-					cs1.amount_work--;
-					cout << "Новое количество рабочих цехов: " << cs1.amount_work << endl;
-				}
-				else
-				{
-					cout << "Ошибка" << endl;
-				}
+				cout << "Нельля прекратить работу цехов!" << endl;
 			}
 		}
 	}
+ 
+void savePIPE_into_file(Pipe pipe)
+{
+	ofstream outf("OutPut_Pipe.txt");
+	if (!outf.is_open())
+		cout << "Файл не может быть открыт!\n";
 	else
-	{
-		cout << "Вы забыли ввести данные для КС!" << endl;
+	{ 
+	outf << "Труба" << endl; 
+	outf << "Идентификатор: " << pipe.id << endl;
+	outf << "Длина: " << pipe.length << endl;
+	outf << "Диаметр: " << pipe.diam << endl;
+	outf << "Ремонт: " << pipe.repair << endl;
+	outf.close();
 	}
-
 }
 
-void output(Pipe pipe1, CS cs1)
+void saveCS_into_file(CS cs)
 {
-	ofstream outf("OutPut.txt");
-	if (pipe1.exist == true)
-	{
-		outf << "Труба" << endl;
-		outf << "Идентификатор: " << pipe1.id << endl;
-		outf << "Длина: " << pipe1.length << endl;
-		outf << "Диаметр: " << pipe1.diam << endl;
-		outf << "Ремонт: " << pipe1.repair << endl;
-	}
+	ofstream outf("OutPut_CS.txt");
+	if (!outf.is_open())
+		cout << "Файл не может быть открыт!\n";
 	else
-	{
-		cout << "Вы забыли ввести данные для трубы!" << endl;
-		outf << "Вы забыли ввести данные для трубы!" << endl;
-	}
-	if (cs1.exist == true)
 	{
 		outf << endl;
 		outf << "Компрессорная станция" << endl;
-		outf << "Идентификатор: " << cs1.id << endl;
-		outf << "Название: " << cs1.name << endl;
-		outf << "Количество цехов: " << cs1.amount << endl;
-		outf << "Количество рабочих цехов: " << cs1.amount_work << endl;
-		outf << "Эффективность: " << cs1.perfomance << endl;
+		outf << "Идентификатор: " << cs.id << endl;
+		outf << "Название: " << cs.name << endl;
+		outf << "Количество цехов: " << cs.amount << endl;
+		outf << "Количество рабочих цехов: " << cs.amount_work << endl;
+		outf << "Эффективность: " << cs.perfomance << endl;
 	}
-	else
-	{
-		cout << "Вы забыли ввести данные для КС!" << endl;
-		outf << "Вы забыли ввести данные для КС!";
-	}
-	outf.close();
 }
 
-void input(Pipe& pipe1, CS& cs1)
+void load_from_file(Pipe& pipe, CS& cs)
 {
 	ifstream fin("InPut.txt");
-	pipe1.exist = true;
-	cs1.exist = true;
 	if (!fin.is_open())
 		cout << "Файл не может быть открыт!\n";
 	else
 	{
-		fin >> pipe1.id >> pipe1.length >> pipe1.diam >> pipe1.repair;
-		fin >> cs1.id >> cs1.name >> cs1.amount >> cs1.amount_work >> cs1.perfomance;
+		fin >> pipe.id >> pipe.length >> pipe.diam >> pipe.repair;
+		fin >> cs.id >> cs.name >> cs.amount >> cs.amount_work >> cs.perfomance;
+		fin.close();
 	}
-	fin.close();
 }
-
-
 
 void menu()
 {
 	system("cls");
 	cout << "1.Добавить трубу" << endl;
 	cout << "2.Добавить КС" << endl;
-	cout << "3.Просмотр всех объектов" << endl;
-	cout << "4.Редактировать трубу" << endl;
-	cout << "5.Редактировать КС" << endl;
-	cout << "6.Сохранить" << endl;
-	cout << "7.Загрузить" << endl;
+	cout << "3.Просмотр информации о трубе" << endl;
+	cout << "4.Просмотр информации о КС" << endl;
+	cout << "5.Редактировать трубу" << endl;
+	cout << "6.Редактировать КС" << endl;
+	cout << "7.Сохранить трубу" << endl;
+	cout << "8.Сохранить КС" << endl;
+	cout << "9.Загрузить" << endl;
 	cout << "0.Выход" << endl;
 	cout << "Введите команду: ";
-
-}
-
-int GetCorrectNumber(int left, int right)
-{
-	int x;
-	while (((cin >> x)).fail() || x<left || x>right)
-	{
-		cin.clear();
-		cin.ignore(1000, '\n');
-		cout << "Ошибка при вводе!" << endl << "Введите команду: ";
-	}
-	return x;
 }
 
 int main()
 {
 	setlocale(LC_ALL, "Russian");
-	Pipe pipe1;
-	CS cs1;
+	Pipe pipe;
+	CS cs;
+	bool pipe_exist = false;
+	bool cs_exist = false;
 	while (true)
 	{
 		menu();
-		switch (GetCorrectNumber(0,7))
+		switch (GetCorrectNumber(0,9))
 		{
 		case 0:
 		{
@@ -280,41 +214,60 @@ int main()
 		}
 		case 1:
 		{
-			addpipe(pipe1);
+			addpipe(pipe);
+			pipe_exist = true;
 			break;
 		}
 		case 2:
 		{
-			addcs(cs1);
+			addcs(cs);
+			cs_exist = true;
 			break;
+			
 		}
 		case 3:
 		{
-			view(pipe1, cs1);
+			view_pipe(pipe);
 			system("Pause");
+			pipe_exist = true;
 			break;
 		}
 		case 4:
 		{
-			editing(pipe1);
+			view_cs(cs);
 			system("Pause");
+			cs_exist = true;
 			break;
 		}
 		case 5:
 		{
-			ceh(cs1);
+			if (pipe_exist) editing_pipe(pipe); else cout << "Вы забыли ввести данные трубы!";
 			system("Pause");
 			break;
 		}
 		case 6:
 		{
-			output(pipe1, cs1);
+			if (cs_exist) editing_cs(cs); else cout << "Вы забыли ввести данные КС!";
 			system("Pause");
 			break;
 		}
 		case 7:
 		{
-			input(pipe1, cs1);
+			if (pipe_exist) savePIPE_into_file(pipe); else cout << "Вы забыли ввести данные для трубы! \n";
+			system("Pause");
+			break;
+		}
+		case 8:
+		{
+			if (cs_exist) saveCS_into_file(cs); else cout << "Вы забыли ввести данные для КС! \n";
+			system("Pause");
+			break;
+		}
+		case 9:
+		{
+			load_from_file(pipe, cs);
+			pipe_exist = true;
+			cs_exist = true;
 			break;
 		}
 		}
